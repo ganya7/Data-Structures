25c89f687a283c8e8e5b31f3ae32a4e64b784d3d
ganya7 <30626883+ganya7@users.noreply.github.com>
2017-12-27 12:02:52 +0530

    Add files via upload

---
+++ fibonaccidp.py
@@ -0,0 +1,15 @@
+def fibonacci(n,lookup):
+	if n==0 or n==1:
+		lookup[n] = n
+	if lookup[n] is None:
+		lookup[n] = fibonacci(n-1,lookup) + fibonacci(n-2,lookup);
+	return lookup[n]
+
+def main():
+	n = 6
+	lookup = [None]*10  #initialize lookup of size 10 with value None = null, None is null is py3
+	print("Fibonacci of n is ",end=" ")
+	print(fibonacci(n,lookup))
+
+if __name__ == '__main__':  #this ensures the program or source file is executed when the file is executed and not imported
+	main()
